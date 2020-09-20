3a5294303a52448de8b9b51e773e054d24516f6b
cle-b <cle@tictac.pm>
2020-09-20 18:58:29 +0200

    allow to run scripts and pytest tests (#5)

--- httpdbg/__main__.py
+++ httpdbg/__main__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import code
+import importlib.util
 import readline  # noqa: F401 enable the 'up arrow' history in the console
+import sys
 
 from .httpdbg import httpdbg
 
@@ -16,9 +18,38 @@ def console():
         pass
 
 
-if __name__ == "__main__":
+def pyhttpdbg():
 
-    print("httpdbg - recorded requests available at http://localhost:5000/ ")
+    print("-- -- -- httpdbg - recorded requests available at http://localhost:5000/ ")
+
+    argv = sys.argv[1:]
 
     with httpdbg():
-        console()
+        if len(argv) == 0:
+            console()
+        elif argv[0] == "pytest":
+            sys.argv = argv
+            try:
+                import pytest
+
+                pytest.main()
+            except Exception as exp:
+                print(exp)
+            input(
+                "-- -- -- httpdbg - recorded requests available at http://localhost:5000/ until you press enter"
+            )
+        else:
+            sys.argv = argv
+            try:
+                spec = importlib.util.spec_from_file_location("torun", argv[0])
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
+            except Exception as exp:
+                print(exp)
+            input(
+                "-- -- -- httpdbg - recorded requests available at http://localhost:5000/ until you press enter"
+            )
+
+
+if __name__ == "__main__":
+    pyhttpdbg()
