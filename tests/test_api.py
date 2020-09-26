08f4394213467443a4aaa09203059b48478941a1
cle-b <cle@tictac.pm>
2020-09-26 17:43:01 +0200

    add new test for api request

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 import requests
 
+import pytest
+
 from utils import _run_under_httpdbg
 
 
+@pytest.mark.api
+@pytest.mark.requests
 def test_api_requests_one_request(httpbin):
     def _test(httpbin):
         requests.get(httpbin.url + "/get")
 
     stop_httpdbg, current_httpdbg_port = _run_under_httpdbg(_test, httpbin)
 
-    ret = requests.get("http://127.0.0.1:%d/requests" % current_httpdbg_port)
+    ret = requests.get(f"http://127.0.0.1:{current_httpdbg_port}/requests")
     stop_httpdbg()
 
     reqs = ret.json()["requests"]
@@ -19,6 +23,8 @@ def test_api_requests_one_request(httpbin):
     assert reqs[0]["uri"] == httpbin.url + "/get"
 
 
+@pytest.mark.api
+@pytest.mark.requests
 def test_api_requests_two_requests(httpbin):
     def _test(httpbin):
         requests.get(httpbin.url + "/get/abc")
@@ -26,7 +32,7 @@ def test_api_requests_two_requests(httpbin):
 
     stop_httpdbg, current_httpdbg_port = _run_under_httpdbg(_test, httpbin)
 
-    ret = requests.get("http://127.0.0.1:%d/requests" % current_httpdbg_port)
+    ret = requests.get(f"http://127.0.0.1:{current_httpdbg_port}/requests")
     stop_httpdbg()
 
     reqs = ret.json()["requests"]
@@ -34,3 +40,143 @@ def test_api_requests_two_requests(httpbin):
     assert len(reqs) == 2
     assert reqs[0]["uri"] == httpbin.url + "/get/abc"
     assert reqs[1]["uri"] == httpbin.url + "/get/def"
+
+
+@pytest.mark.api
+@pytest.mark.request
+def test_api_request_by_id(httpbin):
+    def _test(httpbin):
+        requests.get(httpbin.url + "/get/abc")
+        requests.get(httpbin.url + "/get/def")
+
+    stop_httpdbg, current_httpdbg_port = _run_under_httpdbg(_test, httpbin)
+
+    ret = requests.get(f"http://127.0.0.1:{current_httpdbg_port}/request/0")
+    assert ret.status_code == 200
+    assert ret.json()["uri"] == httpbin.url + "/get/abc"
+
+    ret = requests.get(f"http://127.0.0.1:{current_httpdbg_port}/request/1")
+    assert ret.status_code == 200
+    assert ret.json()["uri"] == httpbin.url + "/get/def"
+
+    stop_httpdbg()
+
+
+@pytest.mark.api
+@pytest.mark.request
+def test_api_request_by_id_not_exists(httpbin):
+    def _test(httpbin):
+        requests.get(httpbin.url + "/get/abc")
+        requests.get(httpbin.url + "/get/def")
+
+    stop_httpdbg, current_httpdbg_port = _run_under_httpdbg(_test, httpbin)
+
+    ret = requests.get(f"http://127.0.0.1:{current_httpdbg_port}/request/999")
+    assert ret.status_code == 404
+
+    stop_httpdbg()
+
+
+@pytest.mark.api
+@pytest.mark.request
+def test_api_get_request_get(httpbin):
+    def _test(httpbin):
+        requests.get(httpbin.url + "/")
+        requests.get(httpbin.url + "/get")
+
+    stop_httpdbg, current_httpdbg_port = _run_under_httpdbg(_test, httpbin)
+
+    ret = requests.get(f"http://127.0.0.1:{current_httpdbg_port}/request/1")
+
+    # headers
+    assert ret.status_code == 200
+    assert ret.json()["uri"] == httpbin.url + "/get"
+    assert ret.json()["method"] == "GET"
+    assert ret.json()["protocol"] == "http"
+    assert ret.json()["status"] == {"code": 200, "message": "OK"}
+
+    # request
+    assert {"name": "Connection", "value": "keep-alive"} in ret.json()["request"][
+        "headers"
+    ]
+    assert ret.json()["request"]["cookies"] == []
+    assert "body" not in ret.json()["request"]
+
+    # response
+    assert {"name": "Content-Type", "value": "application/json"} in ret.json()[
+        "response"
+    ]["headers"]
+    assert ret.json()["response"]["cookies"] == []
+    assert ret.json()["response"]["body"]["filename"] == "get"
+    path_to_content = ret.json()["response"]["body"]["path"]
+    assert (
+        requests.get(
+            f"http://127.0.0.1:{current_httpdbg_port}/{path_to_content}"
+        ).status_code
+        == 200
+    )
+
+    stop_httpdbg()
+
+
+@pytest.mark.api
+@pytest.mark.request
+def test_api_get_request_post(httpbin):
+    def _test(httpbin):
+        requests.get(httpbin.url + "/")
+        requests.post(httpbin.url + "/post", data="data to post")
+
+    stop_httpdbg, current_httpdbg_port = _run_under_httpdbg(_test, httpbin)
+
+    ret = requests.get(f"http://127.0.0.1:{current_httpdbg_port}/request/1")
+
+    # headers
+    assert ret.status_code == 200
+    assert ret.json()["uri"] == httpbin.url + "/post"
+    assert ret.json()["method"] == "POST"
+    assert ret.json()["protocol"] == "http"
+    assert ret.json()["status"] == {"code": 200, "message": "OK"}
+
+    # request
+    assert {"name": "Connection", "value": "keep-alive"} in ret.json()["request"][
+        "headers"
+    ]
+    assert ret.json()["request"]["cookies"] == []
+    assert ret.json()["request"]["body"]["filename"] == "upload"
+    path_to_content = ret.json()["request"]["body"]["path"]
+    assert (
+        requests.get(f"http://127.0.0.1:{current_httpdbg_port}/{path_to_content}").text
+        == "data to post"
+    )
+
+    # response
+    assert {"name": "Content-Type", "value": "application/json"} in ret.json()[
+        "response"
+    ]["headers"]
+    assert ret.json()["response"]["cookies"] == []
+    assert ret.json()["response"]["body"]["filename"] == "post"
+    path_to_content = ret.json()["response"]["body"]["path"]
+    assert (
+        requests.get(
+            f"http://127.0.0.1:{current_httpdbg_port}/{path_to_content}"
+        ).status_code
+        == 200
+    )
+
+    stop_httpdbg()
+
+
+@pytest.mark.api
+@pytest.mark.request
+def test_api_get_request_get_status_404(httpbin):
+    def _test(httpbin):
+        requests.get(httpbin.url + "/get/abc")
+
+    stop_httpdbg, current_httpdbg_port = _run_under_httpdbg(_test, httpbin)
+
+    ret = requests.get(f"http://127.0.0.1:{current_httpdbg_port}/request/0")
+    stop_httpdbg()
+
+    assert ret.status_code == 200
+    assert ret.json()["uri"] == httpbin.url + "/get/abc"
+    assert ret.json()["status"] == {"code": 404, "message": "NOT FOUND"}
--- tests/test_context_manager.py
+++ tests/test_context_manager.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 import requests
 
+import pytest
+
 from httpdbg import httpdbg
 from httpdbg.webapp import httpdebugk7
 
 from utils import httpdbg_port
 
 
+@pytest.mark.cm
 def test_context_manager(httpbin):
 
     assert httpdebugk7["k7"] is None
