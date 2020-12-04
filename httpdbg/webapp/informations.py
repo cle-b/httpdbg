be8e9ca539009b6e6469b692ca5d108e3dfaac78
cle-b <cle@tictac.pm>
2020-12-04 20:03:11 +0100

    add tooltip on status and method

---
+++ httpdbg/webapp/informations.py
@@ -0,0 +1,11 @@
+http_verbs = {
+    "GET": "Transfer a current representation of the target resource.",
+    "HEAD": "Same as GET, but only transfer the status line and header section.",
+    "POST": "Perform resource-specific processing on the request payload.",
+    "PUT": "Replace all current representations of the target resource with the request payload.",
+    "DELETE": "Remove all current representations of the target resource.",
+    "CONNECT": "Establish a tunnel to the server identified by the target resource.",
+    "OPTIONS": "Describe the communication options for the target resource.",
+    "TRACE": "Perform a message loop-back test along the path to the target resource.",
+    "PATCH": "Request that a set of changes described in the request entity be applied to the resource identified by the Request-URI.",
+}
