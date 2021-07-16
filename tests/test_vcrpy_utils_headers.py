2aecdbb13fa8f8546df83c203cffd4c44cc8922d
cle-b <cle@tictac.pm>
2021-07-16 16:43:16 +0200

    linting

--- tests/test_vcrpy_utils_headers.py
+++ tests/test_vcrpy_utils_headers.py
@@ -16,9 +16,7 @@ def test_vcrpy_utils_get_headers_request(httpbin):
 
     with my_vcr.use_cassette(path="cassettes", decode_compressed_response=True) as k7:
         requests.get(httpbin.url + "/get")
-        assert {'name': 'Accept', 'value': '*/*'} in get_headers(
-            k7.requests[0].headers
-        )
+        assert {"name": "Accept", "value": "*/*"} in get_headers(k7.requests[0].headers)
 
 
 @pytest.mark.vcrpy
@@ -44,6 +42,7 @@ def test_vcrpy_utils_get_header_request(httpbin):
         requests.get(httpbin.url + "/get")
         assert "gzip" in get_header(k7.requests[0].headers, "Accept-Encoding")
         assert "gzip" in get_header(k7.requests[0].headers, "ACCEPT-Encoding")
+
 
 @pytest.mark.vcrpy
 def test_vcrpy_utils_get_header_response(httpbin):
