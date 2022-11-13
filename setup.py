e07230cf6b54c6c17b93f2045c9f9d76d94ad297
cle-b <cle@tictac.pm>
2022-11-13 18:40:19 +0100

    new version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="httpdbg",
-    version="0.0.49",
+    version="0.0.50",
     author="cle-b",
     author_email="cle@tictac.pm",
     description="A very simple tool to debug HTTP(S) client requests",
