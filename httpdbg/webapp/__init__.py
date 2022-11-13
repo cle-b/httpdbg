86944c5ba8a6d9fe5fa677f6e1a61e81ae7a9742
cle-b <cle@tictac.pm>
2022-11-13 18:30:10 +0100

    fix import

--- httpdbg/webapp/__init__.py
+++ httpdbg/webapp/__init__.py
@@ -1,4 +1,4 @@
-from httpdbg.webapp.api import HttpbgHTTPRequestHandler
 from httpdbg.webapp.api import httpdebugk7
+from httpdbg.webapp.app import HttpbgHTTPRequestHandler
 
 __all__ = ["HttpbgHTTPRequestHandler", "httpdebugk7"]
