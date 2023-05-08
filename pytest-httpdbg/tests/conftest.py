5f583f8b89fbf44ae2023cea72aa2e1df547f1f4
cle-b <cle@tictac.pm>
2023-05-08 17:00:49 +0200

    fix import for tests

--- pytest-httpdbg/tests/conftest.py
+++ pytest-httpdbg/tests/conftest.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
+import os
+import sys
+
 pytest_plugins = ["pytester"]
+
+
+if sys.version_info < (3, 7):
+    # to fix an import problem with python 3.6 during the tests
+    os.environ["PYTHONPATH"] = os.path.realpath(
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../httpdbg")
+    )
