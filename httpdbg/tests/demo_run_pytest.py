7ee4f110e871b363d103e970fbd79919dcdaf3ba
cle-b <cle@tictac.pm>
2023-05-08 16:01:49 +0200

    add ignored files

---
+++ httpdbg/tests/demo_run_pytest.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import os
+
+import requests
+
+
+def test_demo_pytest():
+    base_url = os.environ["HTTPDBG_TEST_PYTEST_BASE_URL"]
+
+    requests.post(f"{base_url}/post")
+    requests.get(f"{base_url}/get")
+    requests.put(f"{base_url}/put")
+
+
+def test_demo_raise_exception(fixture_which_does_not_exist):
+    pass
