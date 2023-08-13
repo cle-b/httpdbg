b5613e3af9b32ae133d27ebfcdb0a444f38bb359
cle-b <cle@tictac.pm>
2023-08-13 17:15:15 +0200

    add payload into filter

--- httpdbg/webapp/static/filter.js
+++ httpdbg/webapp/static/filter.js
@@ -7,20 +7,28 @@ function update_filter_url() {
         sheet.deleteRule(0);
     }
 
-    var value = document.getElementById("filter-url").value;
+    var value = prepare_for_filter(document.getElementById("filter-url").value);
 
     if (value == "") {
         // if there is no filter, all rows are visible
         sheet.insertRule("[data-filter-url] {display: table-row;}");
+        sheet.insertRule("[data-filter-request] {display: table-row;}");
+        sheet.insertRule("[data-filter-response] {display: table-row;}");
         sheet.insertRule(".initiator-header {display: table-row-group;}");
     } else {
         // if there is a filter, only the requests that match the filter are visible
-        sheet.insertRule("[data-filter-url*='" + value + "'] {display: table-row;}");
+        sheet.insertRule('[data-filter-url*="' + value + '"] {display: table-row;}');
+        sheet.insertRule('[data-filter-request*="' + value + '"] {display: table-row;}');
+        sheet.insertRule('[data-filter-response*="' + value + '"] {display: table-row;}');
         if (CSS.supports("selector(:has(*))")) {
             // an initiator row is visible only if it contains at least one visible request
-            sheet.insertRule(".initiator-header:has([data-filter-url*='" + value + "']) {display: table-row-group;}");
+            sheet.insertRule('.initiator-header:has([data-filter-url*="' + value + '"]) {display: table-row-group;}');
+            sheet.insertRule('.initiator-header:has([data-filter-request*="' + value + '"]) {display: table-row-group;}');
+            sheet.insertRule('.initiator-header:has([data-filter-response*="' + value + '"]) {display: table-row-group;}');
             // if there is a filter and no request visible, the filter icon is red
-            sheet.insertRule(".requests-table:not(:has([data-filter-url*='" + value + "'])):has(tbody) .icon-filter {filter: grayscale(0%) !important;}");
+            sheet.insertRule('.requests-table:not(:has([data-filter-url*="' + value + '"])):has(tbody) .icon-filter {filter: grayscale(0%) !important;}');
+            sheet.insertRule('.requests-table:not(:has([data-filter-request*="' + value + '"])):has(tbody) .icon-filter {filter: grayscale(0%) !important;}');
+            sheet.insertRule('.requests-table:not(:has([data-filter-response*="' + value + '"])):has(tbody) .icon-filter {filter: grayscale(0%) !important;}');
         } else {
             // in case :has is not supported, the initiator rows are always visible
             sheet.insertRule(".initiator-header {display: table-row-group;}");
