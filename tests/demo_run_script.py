d925947dced17957b92a40ac17f6a42c0fd77ab2
cle-b <cle@tictac.pm>
2024-10-28 22:24:34 +0100

    fix script execution (#154)

--- tests/demo_run_script.py
+++ tests/demo_run_script.py
@@ -5,10 +5,11 @@ import requests
 
 base_url = sys.argv[1]
 
-_ = requests.get(f"{base_url}/get")
+if __name__ == "__main__":
+    _ = requests.get(f"{base_url}/get")
 
-if len(sys.argv) == 3:
-    if sys.argv[2] == "raise_exception":
-        raise Exception("--raise_exception--")
+    if len(sys.argv) == 3:
+        if sys.argv[2] == "raise_exception":
+            raise Exception("--raise_exception--")
 
-_ = requests.post(f"{base_url}/post")
+    _ = requests.post(f"{base_url}/post")
