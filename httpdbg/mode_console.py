9e6623a3f6d97d93901d1f42ff8695a3f6604d33
cle-b <cle@tictac.pm>
2024-12-30 18:04:57 +0100

    fix group for mode console (#167)

--- httpdbg/mode_console.py
+++ httpdbg/mode_console.py
@@ -1,13 +1,57 @@
 # -*- coding: utf-8 -*-
 import code
+from typing import List
 from typing import Union
+
+from httpdbg.records import HTTPRecords
+from httpdbg.initiator import httpdbg_group
 
 
 def console_exit():
     raise SystemExit
 
 
-def run_console(test_mode: bool = False) -> Union[code.InteractiveConsole, None]:
+class InteractiveConsoleWithHistory(code.InteractiveConsole):
+
+    def __init__(self, records: HTTPRecords, locals=None):
+        self.records: HTTPRecords = records
+        self.history: List[str] = []
+        self.incomplete_block: bool = False
+        super().__init__(locals)
+
+    def push(self, line):
+        if line:  # if the line is empty, we don't add it in the history
+            if (
+                self.incomplete_block
+            ):  # if the latest instruction is part of a block, we group it with the previous one
+                self.history[-1] += (
+                    "\n          " + line
+                )  # to simulate the block indentation
+            else:
+                self.history.append(line)
+
+        # if the latest instruction is part of a block, the label is generic
+        label = self.history[-1] if not self.incomplete_block else "(block)"
+
+        # in the full label, we keep the last 5 instructions/blocks
+        latest_history = self.history[-5:]
+        if not self.incomplete_block:
+            # in case of a block, we don't know which line within the block is the initiator.
+            # this is why we do not mark it in that case.
+            # this information is displayed in the initiator tooltip of the requests (in console, line X,)
+            latest_history[-1] += " <===="
+
+        full_label = ">>> " + "\n>>> ".join(latest_history)
+
+        with httpdbg_group(self.records, label, full_label):
+            self.incomplete_block = super().push(line)
+
+        return self.incomplete_block
+
+
+def run_console(
+    records: HTTPRecords, test_mode: bool = False
+) -> Union[InteractiveConsoleWithHistory, None]:
     try:
         vars = globals()
         vars.update(locals())
@@ -25,7 +69,7 @@ def run_console(test_mode: bool = False) -> Union[code.InteractiveConsole, None]
 
         vars.update({"exit": console_exit})
 
-        new_console = code.InteractiveConsole(vars)
+        new_console = InteractiveConsoleWithHistory(records, vars)
         if not test_mode:
             new_console.interact()  # pragma: no cover
         else:
