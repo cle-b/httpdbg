870716776cc5ce1805cd47df7dbdf7c802e13637
cle-b <cle@tictac.pm>
2025-01-02 11:57:43 +0100

    fix script execution

--- httpdbg/mode_script.py
+++ httpdbg/mode_script.py
@@ -4,6 +4,8 @@ import sys
 import traceback
 from typing import List
 
+from httpdbg.log import logger
+
 
 def run_script(argv: List[str]) -> None:
     sys.argv = argv
@@ -15,6 +17,15 @@ def run_script(argv: List[str]) -> None:
     except AttributeError:
         exit("script mode - error - the first argument shall be a python file")
     try:
+        original__main___module = sys.modules["__main__"]
+        sys.modules["__main__"] = module  # mandatory to execute a unittest test file.
         spec.loader.exec_module(module)  # type: ignore
     except Exception:
         traceback.print_exc()
+    except SystemExit as ex:
+        # in some case, for example when executing unittest tests from a script,
+        # the SystemExit exception is raised by the script itself so we catch it.
+        # TODO propagate the SystemExit value when httpdbg terminates.
+        logger().info(f"run_script - exception SystemExit - {str(ex)}")
+    finally:
+        sys.modules["__main__"] = original__main___module
