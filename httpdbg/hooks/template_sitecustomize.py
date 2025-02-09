ca3de2096766ff1ab001a42f586292d4812b9129
cle-b <cle@tictac.pm>
2025-02-09 20:31:03 +0100

    fix windows

--- httpdbg/hooks/template_sitecustomize.py
+++ httpdbg/hooks/template_sitecustomize.py
@@ -16,7 +16,7 @@ class HttpdbgRecorder:
 
     def __init__(self):
         self._running: bool = False
-        self.fname: str = f"{os.environ['HTTPDBG_MULTIPROCESS_DIR']}/{uuid.uuid1()}"
+        self.fname: str = os.path.join(os.environ['HTTPDBG_MULTIPROCESS_DIR'], uuid.uuid1())
         self.context = None
         self.records: Union[HTTPRecords, None] = None
 
