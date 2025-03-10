439acebb93307ae40c6e9a61431ca54c29101b3a
cle-b <cle@tictac.pm>
2025-03-10 18:03:36 +0100

    fix params for subprocess

--- httpdbg/hooks/external.py
+++ httpdbg/hooks/external.py
@@ -4,7 +4,6 @@ from typing import Generator
 import glob
 import os
 import pickle
-import shutil
 import tempfile
 import time
 import threading
@@ -15,7 +14,9 @@ from httpdbg.records import HTTPRecords
 
 
 @contextmanager
-def watcher_external(records: HTTPRecords) -> Generator[HTTPRecords, None, None]:
+def watcher_external(
+    records: HTTPRecords, initiators: list[str] | None = None, server: bool = False
+) -> Generator[HTTPRecords, None, None]:
     if HTTPDBG_MULTIPROCESS_DIR not in os.environ:
         with tempfile.TemporaryDirectory(prefix="httpdbg_") as httpdbg_multiprocess_dir:
 
@@ -31,7 +32,20 @@ def watcher_external(records: HTTPRecords) -> Generator[HTTPRecords, None, None]
             )
             sitecustomize = os.path.join(httpdbg_multiprocess_dir, "sitecustomize.py")
 
-            shutil.copy(template, sitecustomize)
+            with open(template) as f:
+                template_content = f.read()
+
+            if initiators:
+                template_content = template_content.replace(
+                    "HTTPDBG_INITIATORS = []", f"HTTPDBG_INITIATORS = {initiators}", 1
+                )
+
+            template_content = template_content.replace(
+                "HTTPDBG_RECORD_SERVER = False", f"HTTPDBG_RECORD_SERVER = {server}", 1
+            )
+
+            with open(sitecustomize, "w") as f:
+                f.write(template_content)
 
             if "PYTHONPATH" in os.environ:
                 os.environ["PYTHONPATH"] = (
