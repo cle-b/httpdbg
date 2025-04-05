c8152266483d4876b9b13002cfb443ec59843235
cle-b <cle@tictac.pm>
2025-04-05 15:03:44 +0200

    debug

--- tests/test_server_hook_fastapi.py
+++ tests/test_server_hook_fastapi.py
@@ -33,7 +33,7 @@ def test_fastapi_endpoint(httpdbg_port):
             requests.get(f"http://localhost:{httpdbg_port}/")
 
             # endpoint no parameters
-            assert len(records) == 1
+            assert len(records) == 1, records._print_for_debug()
             record = records[0]
             group = records.groups[record.group_id]
 
