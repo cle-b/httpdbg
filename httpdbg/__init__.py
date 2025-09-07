957ad806f707a33282a101df8c92c6e3d5cac6e4
cle-b <cle@tictac.pm>
2025-09-07 15:35:28 +0200

    new version

--- httpdbg/__init__.py
+++ httpdbg/__init__.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 from httpdbg.hooks.all import httprecord
 from httpdbg.records import HTTPRecords
 
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 
 __all__ = ["httprecord", "HTTPRecords"]
