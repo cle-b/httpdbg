6a3f6f622a363b340e94085b98c514295b13cb57
cle-b <cle@tictac.pm>
2025-09-07 17:37:50 +0200

    example

---
+++ tests/demo_client_load.py
@@ -0,0 +1,17 @@
+import requests
+import statistics
+import time
+
+def run_get(fnc, nb):
+    times = []
+    tb = time.time()
+    for _ in range(nb):
+        t0 = time.time( )
+        assert fnc("http://localhost:8000").content == b'"Hello, World!"'
+        t1 = time.time()
+        times.append(t1-t0)
+    te = time.time()        
+    print(f"min: {int(min(times)*1000)}\t - max: {int(max(times)*1000)}\t - median: {int(statistics.median(times)*1000)}\t - total: {int((te-tb)*1000)}")
+
+if __name__ == "__main__":
+    run_get(requests.get, 1000)
